a9c7cb7e75a24b4bae7cbad3eeb95c6fd60d0dc8
정우재 <80890472+woojae05@users.noreply.github.com>
2021-09-02 20:15:47 +0900

    Create Read.js
    Read기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 
 import './App.css';
-import Create from'./components/Create'
+import { BrowserRouter as Router, Route} from "react-router-dom"
+import Create from'./components/Create';
+import Read from'./components/Read';
+import Update from "./components/Update";
+
+
 function App() {
-
-    return (
+  return (
+    <Router>
       <div className="main">
-        <div className="main-header">React Crud Application with Axios.</div>
-        <Create />
+        <div className="main-header">
+          <div className="header-title">React Crud Application with Axios.</div>
+          <div className="RouteWrapper">
+            <div>
+              <Route exact path="/Create" component={Create}/>
+              <Route exact path="/Read" component={Read}/>
+              <Route exact path="/Update" component={Update}/>
+            </div>
+          </div>
+        </div>
       </div>
-
-    );
+    </Router>
+  );
 }
+
 
 export default App;
---
+++ src/components/Read.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import "../styles/Read.css";
+import axios from "axios";
+
+function Read() {
+  const [APIData, SetAPIData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://612e28b3d11e5c00175583c4.mockapi.io/fakeData`)
+      .then((response) => {
+        // console.log(response.data);
+        SetAPIData(response.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Checked</th>
+        </tr>
+        {APIData.map((data) => {
+          return (
+            <tr>
+              <td>{data.firstName}</td>
+              <td>{data.lastName}</td>
+              <td>{data.checkbox ? "true":"false"} </td>
+            <td><button>edit</button></td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
+}
+
+
+
+export default Read;

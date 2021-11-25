3b9a0e701f434d1f97f5948da8b83ae6c57f5b53
정우재 <80890472+woojae05@users.noreply.github.com>
2021-11-25 21:29:09 +0900

    create: todo
    todoList sever구현

---
+++ mytodo/todo-sever/todo.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors  = require('cors')
+const app = express();
+const port =  8080;
+
+const todos=[
+    {
+        id:1,
+        title:"React Hooks 익히기"
+    },
+    {
+        id:2,
+        title:"GraphQL 익히기"
+    },
+    {
+        id:3,
+        title:'apollo 익히기'
+    },
+]
+
+app.use(cors());
+app.options("*",cors());
+
+app.get("/",(req,res)=>{
+    res.send("hell0")
+})
+
+app.get("/todo",(req,res)=>{
+    res.json(todos);
+})
+
+app.listen(port,()=>console.log(`exapmle app listening on port ${port}`))
